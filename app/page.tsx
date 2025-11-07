62fcf5d824548d5472dac8b704d2d4b57dbeefd9
Puckapao <casval.pak@gmail.com>
2025-11-07 22:11:47 +0700

    update 2.1.2 testnet

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ const CONTRACT_OPTIONS = [
   { key: '2nd', name: '2nd', address: '0xee6d4e937f0493fb461f28a75cf591f1dba8704e' },
   { key: 'adv', name: 'Adversarial', address: '0x216f071653a82ced3ef9d29f3f0c0ed7829c8f81' },
   { key: 'testnet', name: 'Testnet', address: '0x29fa27e173f058d0f5f618f5abad2757747f673f'},
+  { key: '2.1.2 testnet', name: '2.1.2 Testnet', address: '0xebd99ff0ff6677205509ae73f93d0ca52ac85d67'}
 ];
 
 const DEFAULT_RPCS = [
